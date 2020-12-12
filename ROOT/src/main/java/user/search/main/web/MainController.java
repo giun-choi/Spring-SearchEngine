10719951cc0c414171c5f540eb0020b1ee494529
bathr <bathr@DESKTOP-GK26CDH>
2020-12-12 19:02:43 +0900

    main페이지 추가

---
+++ ROOT/src/main/java/user/search/main/web/MainController.java
@@ -0,0 +1,24 @@
+package user.search.main.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 메인 검색 컨트롤러
+ * @author bathr
+ *
+ */
+@Controller
+public class MainController {
+
+	/**
+	 * 메인 페이지
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public String getMain() {
+		
+		return "main";
+	}
+	
+}
